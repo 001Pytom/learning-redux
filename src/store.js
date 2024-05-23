fc75d31f209ec86ca72a6801164fd156f382f3d1
faatihat <tomfaah@gmail.com>
2024-05-23 19:20:35 +0100

    classic redux first

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { combineReducers, createStore } from "redux";
+import accountReducer from "./features/accounts/accountslice";
+import customerReducer from "./features/customers/customerSlice";
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
+export default store;
